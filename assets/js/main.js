69009ed0c205e4b52dc391d914d7939b7d9b0048
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2021-11-29 13:04:45 -0300

    xd

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,84 @@ init
 '.background'
   });
 }
+let contador = 0;
+fetch("https://sheet.best/api/sheets/b9e5c44a-97e5-447b-a248-f9f615e482b6")
+.then((response) => response.json())
+.then((data) => {
+    
+let productos = data;
+console.log(productos); 
+    /*Render*/
+    productos.forEach(element => {
+        contador = contador + 1;
+        $("#marginPrecios").append(`
+        <div class="col-lg-4 col-md-7 col-sm-9 cardPrecios" >
+        <div class="single-pricing text-center wow fadeInUp" data-wow-duration="3s" data-wow-delay="0.2s">
+        <div class="pricin
+        
+        title">
+            <h4 class="title">${element.nombredeltelefono}</h4>
+        </div>
+        <div class="pricing-price">
+        <img src="${element.imagen}" alt="...">
+        </div>
+        <div class="pricing-list">
+            <table class="table table-hover">
+            
+            <tbody id="tbody${contador}">
+                <tr>
+                <td>${element.pantalla}</td>
+                <td>$${element.precioPantalla}</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        
+        </div>
+        </div>
+
+        `);
+        if (element.bateria){
+            $("#tbody" + contador).append(`
+            <tr>
+                <td>${element.bateria}</td>
+                <td>$${element.precioBat}</td>
+                </tr>
+            `);
+        }
+        if (element.placa){
+            $("#tbody" + contador).append(`
+            <tr>
+                <td>${element.placa}</td>
+                <td>$${element.precioPlaca}</td>
+                </tr>
+            `);
+        }
+        if (element.vidrioTrasero){
+            $("#tbody" + contador).append(`
+            <tr>
+                <td>${element.vidrioTrasero}</td>
+                <td>$${element.precioVidrio}</td>
+                </tr>
+            `);
+        }
+        if (element.puertoCarga){
+            $("#tbody" + contador).append(`
+            <tr>
+                <td>${element.puertoCarga}</td>
+                <td>$${element.precioPuerto}</td>
+                </tr>
+            `);
+        }
+        if (element.botonHome){
+            $("#tbody" + contador).append(`
+            <tr>
+                <td>${element.botonHome}</td>
+                <td>$${element.precioBoton}</td>
+                </tr>
+            `);
+        }
+    });    
+
+})
+
